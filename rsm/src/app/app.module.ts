ceed6c8fa2b0971eef5fdd2328bf71459971a987
Thomas Brunbjerg <thomb@hotmail.dk>
2017-05-18 14:01:25 +0200

    Tilføjet laug component

--- rsm/src/app/app.module.ts
+++ rsm/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LaugComponent } from './laug/laug.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LaugComponent
   ],
   imports: [
     BrowserModule,
